0c7f3952c78477298d2f2e3f0bd561c70e6df8ca
vishal31raj <vraj07909@gmail.com>
2019-01-05 13:58:57 +0530

    Add GET/users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,9 +8,10 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -24,73 +27,92 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req,res) => {
+app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
-app.get('/todos/:id', (req,res) => {
+app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)) {
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
-    if(!todo) {
-        return res.status(404).send();
+    if (!todo) {
+      return res.status(404).send();
     }
-    res.status(200).send({todo});
-  }, (e) => {
+
+    res.send({todo});
+  }).catch((e) => {
     res.status(400).send();
   });
 });
 
-app.delete('/todos/:id', (req,res) => {
+app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)) {
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
   Todo.findByIdAndRemove(id).then((todo) => {
-    if(!todo) {
+    if (!todo) {
       return res.status(404).send();
     }
-    res.status(200).send({todo});
-  }, (e) => {
-    res.status(400).send();
 
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
-
-app.patch('/todos/:id', (req,res) => {
+app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
-  if(!ObjectID.isValid(id)) {
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  
-  if(_.isBoolean(body.completed) && body.completed) {
+
+  if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then ((todo) => {
-    if(!todo) {
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
-    }).catch((e) => {
-      res.status(400).send();
-    });
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -17,32 +17,35 @@ const todos = [{
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
-        return Todo.insertMany(todos);
-    }).then (() => done());
-});
+      return Todo.insertMany(todos);
+    }).then(() => done());
+  });
 
 describe('POST /todos', () => {
 
     it('should create a new todo', (done) => {
         var text = 'Test todo text';
-
+    
         request(app)
-        .post('/todos')
-        .send({text})
-        .expect(200)
-        .expect((res) => {
+          .post('/todos')
+          .send({text})
+          .expect(200)
+          .expect((res) => {
             expect(res.body.text).toBe(text);
-        }).end((err,res) => {
-            if(err) {
-                return done(err);
+          })
+          .end((err, res) => {
+            if (err) {
+              return done(err);
             }
+    
             Todo.find({text}).then((todos) => {
-                expect(todos.length).toBe(1);
-                expect(todos[0].text).toBe(text);
-                done();
-            });
-        });
-    });
+              expect(todos.length).toBe(1);
+              expect(todos[0].text).toBe(text);
+              done();
+            }).catch((e) => done(e));
+          });
+      });
+    
 
     it('should not create todo with invalid body data', (done) => {
         request(app)
